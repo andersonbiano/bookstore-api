c90c1df2341b92d18d32c162fee090cc4215a19c
anderson-biano <andersonbiano2018@gmail.com>
2022-07-20 17:42:55 -0300

    Criação da Exceção de Erro

--- src/main/java/com/example/demo/service/CategoriaService.java
+++ src/main/java/com/example/demo/service/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.example.demo.domain.Categoria;
 import com.example.demo.repositories.CategoriaRepository;
+import com.example.demo.service.exception.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -15,7 +16,7 @@ public class CategoriaService {
 	
 	public Categoria findById(Integer id) {
 		Optional<Categoria> categoria = repository.findById(id);
-		return categoria.orElse(null);
+		return categoria.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado: id:" + id + "Tipo: " + Categoria.class.getName()));
 		
 	}
 
